27c2afff35419933658e77a121571e6aa5bfe787
Marcos <marcosvazq033@gmail.comecho>
2023-02-06 09:06:51 +0100

    Creacion de producto y productoDAO

---
+++ eCommerce/src/main/java/dao/ProductoDao.java
@@ -0,0 +1,84 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import beans.Producto;
+import conex.BDConex;
+
+public class ProductoDao {
+	
+	private static BDConex conex = new BDConex();
+	
+	public static ArrayList<Producto> getProductos() {
+		
+		String sql = "select * from producto";
+		
+		ArrayList<Producto> listaProductos = new ArrayList<Producto>();
+		
+		try (Connection con = conex.getConnection()) {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String nombre = rs.getString("nombre");
+				String descripcion = rs.getString("descripcion");
+				String imagen = rs.getString("imagen");
+				double precio = rs.getDouble("precio");
+				
+				Producto producto = new Producto(id, nombre, descripcion, imagen, precio);
+				
+				listaProductos.add(producto);
+			}
+			
+			rs.close();
+			st.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return listaProductos;
+	}
+	
+	public static Producto getProducto(Integer id) {
+		
+		String sql = "select * from producto where id = ?";
+		
+		Producto producto = null;
+		
+		try (Connection con = conex.getConnection()) {
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			ps.setInt(1, id);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			rs.next();
+			
+			int idP = rs.getInt("id");
+			String nombre = rs.getString("nombre");
+			String descripcion = rs.getString("descripcion");
+			String imagen = rs.getString("imagen");
+			double precio = rs.getDouble("precio");
+			
+			producto = new Producto(idP, nombre, descripcion, imagen, precio);
+			
+			rs.close();
+			ps.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return producto;
+	}
+	
+	
+	
+}
---
+++ eCommerce/src/main/webapp/pages/producto.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Ventana</title>
+</head>
+<body>
+
+	<c:if test="${item != null}">
+	
+		<div class="card mb-3" style="max-width: 540px;">
+		  <div class="row g-0">
+		    <div class="col-md-4">
+		      <img src="${item.imagen}" class="img-fluid rounded-start" alt="${item.nombre}">
+		    </div>
+		    <div class="col-md-8">
+		      <div class="card-body">
+		        <h5 class="card-title">${item.nombre}</h5>
+		        <p class="card-text">${item.descripcion}</p>
+		        <p class="card-text"><small class="text-muted">${item.precio}€</small></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+		
+	</c:if>
+
+</body>
+</html>
